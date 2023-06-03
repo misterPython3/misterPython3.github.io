874c0f5884c3b4087f0be58221fd72edb5cc78d7
misterPython3 <genesismatic03@gmail.com>
2023-06-04 04:42:14 +0800

    commit this

--- test.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-
-const firstWordRegex = /^[a-z]+/;
-const camelCaseRegex = /(?<=^[a-z]+.*)[A-Z][a-z]*(?=[A-Z]|$)/g;
-function camelCaseToKebab(string){
-    return camelCaseRegex.test(string) ? string.match(camelCaseRegex).reduce((prev, curr) => `${prev}-${curr.toLowerCase()}`,string.match(firstWordRegex)[0]) : string.toLowerCase();
-}
